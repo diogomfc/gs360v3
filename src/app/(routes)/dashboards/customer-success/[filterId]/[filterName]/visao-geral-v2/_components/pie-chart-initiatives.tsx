342220deed37046be0de60a6045d2d6cdb9f7ce8
Diogo Silva <diogomfc@hotmail.com>
2024-10-10 10:01:53 -0300

    feat: Adicionar arquivos e componentes iniciais
    Adiciona os seguintes arquivos e componentes iniciais:
    - src/app/page.tsx
    - src/lib/react-query.ts
    - postcss.config.mjs
    - src/hooks/atoms.ts
    - src/lib/utils.ts
    - src/components/ui/aspect-ratio.tsx
    - next.config.mjs
    - src/components/ui/skeleton.tsx
    - src/components/ui/collapsible.tsx
    - .gitignore
    - src/app/(routes)/dashboards/data/schema.ts
    - components.json
    - src/app/(routes)/layout.tsx
    - src/app/layout.tsx
    - tsconfig.json
    - biome.json
    - public/assets/customer-success/logo-cielo.svg
    - src/types/environment.d.ts
    - src/app/providers.tsx
    - src/components/ui/toaster.tsx
    - src/components/ui/textarea.tsx
    - src/components/ui/label.tsx
    
    Essas alterações adicionam os arquivos e componentes iniciais ao projeto, fornecendo a estrutura básica para o desenvolvimento.

---
+++ src/app/(routes)/dashboards/customer-success/[filterId]/[filterName]/visao-geral-v2/_components/pie-chart-initiatives.tsx
@@ -0,0 +1,138 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import * as React from 'react';
+import { Label, Pie, PieChart } from 'recharts';
+
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import {
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from '@/components/ui/chart';
+import { statuses } from '../../../../../data';
+
+// Definindo o chartConfig para o gráfico
+const chartConfig = {
+	iniciativa: {
+		label: 'Iniciativa',
+	},
+	backlog: {
+		label: 'Backlog',
+		color: statuses.find((status) => status.value === 'Backlog')?.color,
+	},
+	impedimento: {
+		label: 'Impedimento',
+		color: statuses.find((status) => status.value === 'Impedimento Onboarding')
+			?.color,
+	},
+	concluido: {
+		label: 'Concluído',
+		color: statuses.find((status) => status.value === 'Concluído')?.color,
+	},
+	andamento: {
+		label: 'Em andamento',
+		color: statuses.find((status) => status.value === 'Em andamento')?.color,
+	},
+};
+
+export function PieChartInitiatives({ jiraData }: any) {
+	const chartData = React.useMemo(() => {
+		return [
+			{
+				status: 'Backlog',
+				iniciativa:
+					(jiraData?.statusCounts?.Backlog || 0) +
+					(jiraData?.statusCounts?.['Tarefas Onboarding'] || 0) +
+					(jiraData?.statusCounts?.['Tarefas Ongoing'] || 0) +
+					(jiraData?.statusCounts?.['Tarefas Suporte'] || 0),
+				fill: 'var(--color-backlog)',
+			},
+			{
+				status: 'Impedimento',
+				iniciativa:
+					(jiraData?.statusCounts?.['Impedimento Onboarding'] || 0) +
+					(jiraData?.statusCounts?.['Impedimento Ongoing'] || 0) +
+					(jiraData?.statusCounts?.['Impedimento Suporte'] || 0),
+				fill: 'var(--color-impedimento)',
+			},
+			{
+				status: 'Concluído',
+				iniciativa: jiraData?.statusCounts?.Concluído || 0,
+				fill: 'var(--color-concluido)',
+			},
+			{
+				status: 'Em andamento',
+				iniciativa: jiraData?.statusCounts?.['Em andamento'] || 0,
+				fill: 'var(--color-andamento)',
+			},
+		];
+	}, [jiraData]);
+
+	const totalIniciativas = chartData.reduce(
+		(acc, curr) => acc + curr.iniciativa,
+		0,
+	);
+
+	return (
+		<div className="flex flex-col">
+			<CardHeader className="items-center pb-0">
+				<h1 className="flex flex-col items-center font-semibold">
+					Total de Iniciativas
+				</h1>
+			</CardHeader>
+			<CardContent className="flex-1 pb-0">
+				<ChartContainer
+					config={chartConfig}
+					className="mx-auto aspect-square max-h-[250px]"
+				>
+					<PieChart>
+						<ChartTooltip
+							cursor={true}
+							content={
+								<ChartTooltipContent
+									className="w-[200px]"
+									indicator="line"
+									hideLabel
+								/>
+							}
+						/>
+						<Pie
+							data={chartData}
+							dataKey="iniciativa"
+							nameKey="status"
+							innerRadius={50}
+							strokeWidth={5}
+						>
+							<Label
+								content={({ viewBox }) => {
+									if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+										return (
+											<text
+												x={viewBox.cx}
+												y={viewBox.cy}
+												textAnchor="middle"
+												dominantBaseline="middle"
+											>
+												<motion.tspan
+													x={viewBox.cx}
+													y={viewBox.cy}
+													className="fill-foreground text-2xl font-bold text-gray-800"
+													initial={{ opacity: 0, y: 20 }}
+													animate={{ opacity: 1, y: 0 }}
+													transition={{ duration: 0.5, delay: 0.5 }}
+												>
+													{totalIniciativas}
+												</motion.tspan>
+											</text>
+										);
+									}
+								}}
+							/>
+						</Pie>
+					</PieChart>
+				</ChartContainer>
+			</CardContent>
+		</div>
+	);
+}
